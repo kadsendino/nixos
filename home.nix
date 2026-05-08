ea94c2e63028d13b51bc52953e9b4025d2bdaa96
kadsendino <mail@kadsendino.org>
2026-05-08 20:57:42 +0200

    added fish config

--- home.nix
+++ home.nix
@@ -40,7 +40,7 @@
   mkdir -p $HOME/Bilder
   mkdir -p $HOME/.config
 
-  ln -sfn /etc/nixos/ $HOME/nixos
+  # ln -sfn /etc/nixos/ $HOME/nixos
   ln -sfn /etc/nixos/dotfiles/wallpaper/ $HOME/Bilder/wallpaper
   ln -sfn /etc/nixos/dotfiles/noctalia/ $HOME/.config/noctalia
   ln -sfn /etc/nixos/dotfiles/niri/ $HOME/.config/niri
